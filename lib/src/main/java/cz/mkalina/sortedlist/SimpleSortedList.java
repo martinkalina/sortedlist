a83b289b6d04832a70d37e586e076b02c7f4a248
martinkalina <martin.kalina@seznam.cz>
2025-03-12 15:59:28 +0100

    Initial commit

---
+++ lib/src/main/java/cz/mkalina/sortedlist/SimpleSortedList.java
@@ -0,0 +1,66 @@
+package cz.mkalina.sortedlist;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This is a thin wrapper over LinkedList, which keeps values sorted.
+ *
+ * @param <T>
+ */
+public class SimpleSortedList<T extends Comparable<T>> extends LinkedList<T> {
+
+    public SimpleSortedList() {
+        super();
+    }
+    public SimpleSortedList(Collection<T> collection) {
+        addAll(collection);
+    }
+
+    @Override
+    public boolean add(T t) {
+        Iterator<T> iterator = iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            T next = iterator.next();
+            if (next.compareTo(t) > 0) {
+                super.add(i, t);
+                return true;
+            }
+            i++;
+        }
+        super.addLast(t);
+        return true;
+    }
+
+
+    @Override
+    public void add(int index, T element) {
+        throw new UnsupportedOperationException();
+    }
+
+    // following methods does not make sense for list with fixed items order
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        c.forEach(this::add);
+        return true;
+    }
+
+    @Override
+    public void addFirst(T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addLast(T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ lib/src/test/java/cz/mkalina/sortedlist/SortedListTest.java
@@ -0,0 +1,70 @@
+package cz.mkalina.sortedlist;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class SortedListTest {
+
+    @ParameterizedTest
+    @MethodSource("lists")
+    public <T extends Comparable<T>> void testGet(List<T> testData) {
+        List<T> sortedList = new SimpleSortedList<>(testData);
+        List<T> sortedData = sortData(testData);
+        for (int i = 0; i < sortedData.size(); i++) {
+            assertEquals(sortedData.get(i), sortedList.get(i));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("lists")
+    public <T extends Comparable<T>> void testAdd(List<T> testData) {
+        List<T> sortedList = new SimpleSortedList<>();
+        testData.forEach(sortedList::add);
+        assertEquals(sortData(testData), sortedList);
+    }
+
+    @ParameterizedTest
+    @MethodSource("lists")
+    public <T extends Comparable<T>> void testAddAll(List<T> testData) {
+        List<T> sortedList = new SimpleSortedList<>();
+        sortedList.addAll(testData);
+        assertEquals(sortData(testData), sortedList);
+    }
+
+    @ParameterizedTest
+    @MethodSource("lists")
+    public <T extends Comparable<T>> void testCreate(List<T> testData) {
+        List<T> sortedList = new SimpleSortedList<>(testData);
+        assertEquals(sortData(testData), sortedList);
+    }
+
+    @ParameterizedTest
+    @MethodSource("lists")
+    public <T extends Comparable<T>> void testSize(List<T> testData) {
+        List<T> sortedList = new SimpleSortedList<>();
+        sortedList.addAll(testData);
+        assertEquals(testData.size(), sortedList.size());
+    }
+
+    private <T extends Comparable<T>> List<T> sortData(List<T> testData) {
+        return testData.stream().sorted().toList();
+    }
+
+    public static Stream<Arguments> lists() {
+        return Stream.of(
+                arguments(List.of()),
+                arguments(List.of(1)),
+                arguments(List.of(3, 2, 1)),
+                arguments(List.of(3, 2, 2, 1)),
+                arguments(List.of(3, 3, 2, 2, 1, 1)),
+                arguments(List.of("Bob", "Alice", "John", "Clara"))
+        );
+    }
+}
